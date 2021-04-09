e4fdd6c4fda0f753386a8ffd5498ae61be14ee79
robert <robert@roberts-MacBook-Air.local>
2021-04-09 22:49:43 +1000

    clean up

--- main.py
+++ main.py
@@ -58,52 +58,37 @@ def generate_pos(angle, points, p):
     y_pos = int(projected2d[1][0] * scale) + height//2
     return x_pos, y_pos
 
+def generate_attractors(number, beta, rho, sigma, time, ode):
+    attractors = []
+    for n in range(number):
+        r = random.random
+        x, y, z = r(), r(), r()
+        c1, c2, c3 = int(r() * 255), int(r() * 255), int(r() * 255)
+        color = [c1, c2, c3]
+        a = Attractor(x, y, z, beta, rho, sigma, time, ode, color)
+        attractors.append(a)
+    return attractors
 
+def draw_attractor(attractor, attractor_limit):
+    if len(attractor.points) == attractor_limit:
+        attractor.points.pop(0)
+    attractor.next()
+    for p in range(len(attractor.points)):
+        x_pos, y_pos = generate_pos(angle, attractor.points, p)
+        # if attractor.previous is not None: # include this line to view closed attractors "self drawing"
+        if p>0:
+            pygame.draw.line(screen, (attractor.color[0], attractor.color[1], attractor.color[2]), (x_pos, y_pos), attractor.previous, 1) #white
+        attractor.previous=[x_pos, y_pos]
 
-a1 = Attractor(x1, y1, z1, beta, rho, sigma, time, ode)
-a2 = Attractor(x2, y2, z2, beta, rho, sigma, time, ode)
-a3 = Attractor(x3, y3, z3, beta, rho, sigma, time, ode)
-a4 = Attractor(x4, y4, z4, beta, rho, sigma, time, ode)
+attractors = generate_attractors(30, beta, rho, sigma, time, ode)
+
 
 while run:
     screen.fill(black)
-    a1.next()
-    # ic(points1)
-    # ic(a1.points)
-    # a1.next()
-    a2.next()
-    a3.next()
-    a4.next()
-    # points2 = a2.next()
-    # points3 = a3.next()
-    # points4 = a4.next()
-    # if len(points1) == 250:
-    #     a1.points.pop(0)
-    #     a2.points.pop(0)
-    #     a3.points.pop(0)
-    #     a4.points.pop(0)
+    for attractor in attractors:
+        draw_attractor(attractor, 30)
+        #attractor = attractors[a]
 
-
-    # for p in range(int(len(points1)/2),len(points1)):
-    for p in range(len(a1.points)):
-        x_pos1, y_pos1 = generate_pos(angle, a1.points, p)
-        x_pos2, y_pos2 = generate_pos(angle, a2.points, p)
-        x_pos3, y_pos3 = generate_pos(angle, a3.points, p)
-        x_pos4, y_pos4 = generate_pos(angle, a4.points, p)
-        # x_pos2, y_pos2 = generate_pos(angle, points2, p)
-
-        if p > 1:
-            pygame.draw.line(screen, (255,255,255), (x_pos1, y_pos1), previous1, 1) #white
-            pygame.draw.line(screen, (200,140,0), (x_pos2, y_pos2), previous2, 1) #gold
-            pygame.draw.line(screen, (0,60,200), (x_pos3, y_pos3), previous3, 1) #blue
-            pygame.draw.line(screen, (255,222,0), (x_pos4, y_pos4), previous4, 1) #pink
-
-
-            # pygame.draw.circle(screen, (0,255,255) , (x_pos1, y_pos1), 3) #(hsv2rgb(hue, 1, 1)) 
-        previous1 = (x_pos1, y_pos1)
-        previous2 = (x_pos2, y_pos2)
-        previous3 = (x_pos3, y_pos3)
-        previous4 = (x_pos4, y_pos4)
 
     angle += 0.005
     pygame.display.update()
