1ed4f5b9e464bb7fe2372734cd69faaf16db1bfc
robert-cochran <4916242+robert-cochran@users.noreply.github.com>
2024-05-24 12:57:32 +1000

    updated controller

--- src/controller.py
+++ src/controller.py
@@ -21,31 +21,35 @@ class Controller(object):
                 # TODO - after image/trail effect by painting line of entire 
                 # coord history before screen is wiped next round. as opposed to
                 # current approach which only paints newest prev coord
-                coord_matrix_prv = attractor.get_current_coord_matrix()
-                coord_matrix_cur = attractor.generate_next_coordinate()
-                x_prv, y_prv = \
-                        self.camera.translate_around_y_axis(coord_matrix_prv)
-                x_cur, y_cur = \
-                        self.camera.translate_around_y_axis(coord_matrix_cur)
-                colour = self.model.get_colour_dict(attractor_index)
-                self.view.paint_line(colour["red"],
-                                     colour["green"],
-                                     colour["blue"],
-                                     x_cur,
-                                     y_cur,
-                                     x_prv,
-                                     y_prv,
-                                     self.model.get_width())
-                self.view.paint_circle(colour["red"],
-                                       colour["green"],
-                                       colour["blue"],
-                                       x_cur,
-                                       y_cur,
-                                       self.model.get_width()*2)
+                self.paint_line(attractor, attractor_index)
             self.camera.increase_angle(0.005)
             self.view.update_display()
             # ic((attractor.color[0]-1)%255)
             running = not self.view.check_exit_signal()
             #next(save_screen) 
         self.view.quit()
+
+    def paint_circle(self, attractor, attractor_index):
+        coord_matrix_cur = attractor.generate_next_coordinate()
+        x_cur, y_cur = self.camera.translate_around_y_axis(coord_matrix_cur)
+        colour = self.model.get_colour_dict(attractor_index)
+        red = colour["red"]
+        green = colour["green"] 
+        blue = colour["blue"]
+        width = self.model.get_width()*2
+        self.view.paint_circle(red, green, blue, x_cur, y_cur, width)
+
+    def paint_line(self, attractor, attractor_index):
+        coord_matrix_prv = attractor.get_current_coord_matrix()
+        coord_matrix_cur = attractor.generate_next_coordinate()
+        x_prv, y_prv = self.camera.translate_around_y_axis(coord_matrix_prv)
+        x_cur, y_cur = self.camera.translate_around_y_axis(coord_matrix_cur)
+        colour = self.model.get_colour_dict(attractor_index)
+        red = colour["red"]
+        green = colour["green"] 
+        blue = colour["blue"]
+        width = self.model.get_width()*2
+        self.view.paint_line(red, green, blue, x_cur, y_cur, x_prv, y_prv,width)
+
+
 
